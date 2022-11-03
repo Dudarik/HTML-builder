8fd4a760a01dda8b62ec022b323b703ca2c179b8
Dudarik <Dudarik@yandex.ru>
2022-11-03 13:02:02 +0500

    fix: double quotes to single qoutes according eslint settings

--- 01-read-file/index.js
+++ 01-read-file/index.js
@@ -1,9 +1,9 @@
-const fs = require("fs");
-const path = require("path");
+const fs = require('fs');
+const path = require('path');
 
-const FILENAME = "text.txt";
-const UTF8 = "utf-8";
+const FILENAME = 'text.txt';
+const UTF8 = 'utf-8';
 const PATH = path.join(__dirname, FILENAME);
 const readStream = fs.createReadStream(PATH, UTF8);
 
-readStream.on("data", (chunck) => console.log(chunck));
+readStream.on('data', (chunck) => console.log(chunck));
