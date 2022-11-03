31360bdbd4b3eecd9212f33a5f65e714eb1b232e
Dudarik <Dudarik@yandex.ru>
2022-11-03 15:12:48 +0500

    feat: add solution to task 2

---
+++ 02-write-file/index.js
@@ -0,0 +1,46 @@
+const { stdout, stdin } = process;
+
+const fs = require('fs');
+const path = require('path');
+
+const FILE_NAME = 'output.txt';
+const PATH = path.join(__dirname, FILE_NAME);
+const UTF8 = 'utf-8';
+const HELLO_STR = 'Hello Friend! Please input text here: ';
+const BYE_STR = 'Goodbye my friend!';
+
+const readStream = fs.createReadStream(PATH, UTF8);
+
+stdout.write(HELLO_STR);
+
+let fileBody = '';
+
+readStream.on('data', (chunck) => {
+  if (chunck) fileBody += chunck;
+});
+
+readStream.on('error', (err) => {
+  if (err.code === 'ENOENT') {
+    fs.createWriteStream(PATH);
+  }
+});
+
+stdin.on('data', (data) => {
+  const currentString = data.toString().trim();
+
+  if (currentString === 'exit') {
+    console.log(BYE_STR);
+    process.exit();
+  }
+  const writeStream = fs.createWriteStream(PATH);
+
+  fileBody += currentString + '\n';
+
+  writeStream.write(fileBody);
+  stdout.write(HELLO_STR);
+});
+
+process.on('SIGINT', () => {
+  console.log('\n' + BYE_STR);
+  process.exit();
+});
