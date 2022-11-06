c6a6f8bf6ad420ef6f23cbac1c58d9ecd1238092
Dudarik <Dudarik@yandex.ru>
2022-11-06 11:26:32 +0500

    refactor: clear file on every script start, add some logs

--- 02-write-file/index.js
+++ 02-write-file/index.js
@@ -8,9 +8,14 @@ const FILE_NAME = 'output.txt';
 const PATH = path.join(__dirname, FILE_NAME);
 const UTF8 = 'utf-8';
 const HELLO_STR = 'Hello Friend! Please input text here: ';
+const ANOTHER_ONE_STR = 'And another one string, input here: ';
 const BYE_STR = 'Goodbye my friend!';
 
 const readStream = fs.createReadStream(PATH, UTF8);
+
+// Если закоментировать строчку ниже, то output.txt не будет очищаться, при старте скрипта.
+// И данные, введеные тобой, с клавиатуры будут дописываться.
+fs.createWriteStream(PATH).write('');
 
 stdout.write(HELLO_STR);
 
@@ -38,7 +43,7 @@ stdin.on('data', (data) => {
   fileBody += currentString + '\n';
 
   writeStream.write(fileBody);
-  stdout.write(HELLO_STR);
+  stdout.write(ANOTHER_ONE_STR);
 });
 
 process.on('SIGINT', () => {
