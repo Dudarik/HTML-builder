b8a5e651f064893d76dfb3435fb63ab838a8194f
Dudarik <Dudarik@yandex.ru>
2022-11-06 13:43:04 +0500

    refactor: task 4, add recursive copy

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,61 +1,47 @@
 const path = require('path');
 
-const { mkdir, unlink, readdir, copyFile } = require('fs/promises');
+const { mkdir, readdir, copyFile, rm } = require('fs/promises');
 
 const SRC_PATH = path.join(__dirname, 'files');
 const DST_PATH = path.join(__dirname, 'files-copy');
 
-const createDir = async (dirname) => {
+const createDir = async (dstPath, recursive = true, force = true) => {
   try {
-    await mkdir(dirname, { recursive: true });
+    await rm(dstPath, { recursive, force });
+    await mkdir(dstPath);
   } catch (error) {
     console.error(error);
-    return false;
-  }
-  return dirname;
-};
-
-const clearDir = async (dirname) => {
-  try {
-    const files = await readdir(dirname);
-
-    if (files) {
-      for (const file of files) {
-        await unlink(path.join(dirname, file));
-      }
-    }
-  } catch (error) {
-    console.log(error);
     return false;
   }
   return true;
 };
 
 const copyFiles = async (srcPath, dstPath) => {
-  const targetDir = await createDir(dstPath);
-  console.log('\n# Create target folder, if not exist...');
+  const files = await readdir(srcPath, {
+    withFileTypes: true,
+  });
 
-  if (targetDir) {
-    await clearDir(targetDir);
-
-    console.log('# Clear target folder...');
-
-    const srcFiles = await readdir(srcPath);
-    console.log('# Read files form source folder...');
-
-    if (srcFiles) {
-      console.log('# Copy files to destination folder...');
-
-      for (const file of srcFiles) {
-        try {
-          await copyFile(path.join(srcPath, file), path.join(targetDir, file));
-        } catch (error) {
-          console.error(error);
-        }
+  if (files) {
+    for (const file of files) {
+      if (file.isDirectory()) {
+        await mkdir(path.join(dstPath, file.name));
+        await copyFiles(
+          path.join(srcPath, file.name),
+          path.join(dstPath, file.name)
+        );
+      } else {
+        await copyFile(
+          path.join(srcPath, file.name),
+          path.join(dstPath, file.name)
+        );
       }
-      console.log(`# Process end! GLHF! Files copy to: ${dstPath}\n`);
     }
   }
 };
 
-copyFiles(SRC_PATH, DST_PATH);
+const run = async (srcPath, dstPath) => {
+  await createDir(dstPath);
+  await copyFiles(srcPath, dstPath);
+};
+
+run(SRC_PATH, DST_PATH);
